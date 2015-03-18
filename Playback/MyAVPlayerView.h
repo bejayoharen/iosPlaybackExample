c9281917124f156c7d882676a4fc449c499343cd
Bjorn Roche <bjorn@xowave.com>
2015-03-18 10:56:12 -0400

    initial commit

---
+++ Playback/MyAVPlayerView.h
@@ -0,0 +1,13 @@
+//
+//  MyAVPlayerView.h
+//  Playback
+//
+//  Created by Bjorn Roche on 3/17/15.
+//  Copyright (c) 2015 Bjorn Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAVPlayerView : UIView
+
+@end
---
+++ Playback/MyAVPlayerView.m
@@ -0,0 +1,21 @@
+//
+//  MyAVPlayerView.m
+//  Playback
+//
+//  Created by Bjorn Roche on 3/17/15.
+//  Copyright (c) 2015 Bjorn Roche. All rights reserved.
+//
+
+#import "MyAVPlayerView.h"
+
+@implementation MyAVPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
